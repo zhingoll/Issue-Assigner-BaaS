8e5be482b81a25791a77bda7faf7bfc97d00fe12
zhingoll <z1823810655@outlook.com>
2024-10-24 19:23:01 +0800

    add issueassign.py

---
+++ IssueAssign.py
@@ -0,0 +1,18 @@
+from tools.file import FileIO
+from model.registry import ModelRegistry 
+
+class IssueAssign:
+    def __init__(self, config) -> None:
+        self.config = config
+        self.train_data = FileIO.read_interact_file(config['train_data'])
+        self.test_data = FileIO.read_interact_file(config['test_data'])
+        print("IssueAssign initialization completed.")
+
+    def execute(self, load_model):
+        try:
+            model_class = ModelRegistry.get_model(self.config['model_name'])
+            model = model_class(self.config, self.train_data, self.test_data)
+            model.execute(load_model)
+        except Exception as e:
+            print(f"Error during model execution: {e}")
+            raise
