4ae59c2ade8d8e3c05fec193ffabdc55d147e80b
zhingoll <z1823810655@outlook.com>
2024-10-25 22:51:45 +0800

    add validate_data

--- IssueAssign.py
+++ IssueAssign.py
@@ -5,13 +5,14 @@ class IssueAssign:
     def __init__(self, config) -> None:
         self.config = config
         self.train_data = FileIO.read_interact_file(config['train_data'])
+        self.validate_data = FileIO.read_interact_file(config['validate_data'])
         self.test_data = FileIO.read_interact_file(config['test_data'])
         print("IssueAssign initialization completed.")
 
     def run(self, load_model):
         try:
             model_class = ModelRegistry.get_model(self.config['model_name'])
-            model = model_class(self.config, self.train_data, self.test_data)
+            model = model_class(self.config, self.train_data,self.validate_data,self.test_data)
             model.run(load_model)
         except Exception as e:
             print(f"Error during model execution: {e}")
