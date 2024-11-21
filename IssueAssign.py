403afeaf740a4a32e44e8b8ed4705371231d1c36
zhingoll <z1823810655@outlook.com>
2024-11-21 22:28:57 +0800

    modify

--- IssueAssign.py
+++ IssueAssign.py
@@ -1,17 +1,15 @@
 from model.registry import ModelRegistry 
 
-
 class IssueAssign:
     def __init__(self, config) -> None:
         self.config = config
         print("IssueAssign initialization completed.")
 
-
-    def run(self, load_model):
+    def run(self,load_model,hetero):
         try:
             model_class = ModelRegistry.get_model(self.config['model_name'])
             model = model_class(self.config)
-            model.run(load_model)
+            model.run(load_model,hetero)
         except Exception as e:
             print(f"Error during model execution: {e}")
             raise
