af4f11c37c298fd6519c619fe1cd00f75b5302fb
zhingoll <z1823810655@outlook.com>
2024-11-24 20:35:59 +0800

    modify comments

--- tools/nlp.py
+++ tools/nlp.py
@@ -8,27 +8,27 @@ from nltk.corpus import stopwords
 
 def clean_text(text:str) -> str:
     '''
-    清理文本数据 -> 得到干净的文本
+    Clean text data -> Obtain clean text
     '''
     text = str(text)
-    # 全小写
+    # Convert to lowercase
     text = text.lower()
-    # 将markdown格式转换成HTML格式
+    # Convert markdown to HTML format
     text = mistune.html(text)
-    # 匹配并去除URL
+    # Match and remove URLs
     url_pattern_my = re.compile(r"(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]")
     text = url_pattern_my.sub(" ", text)
-    # 将HTML转换成字符串
+    # Convert HTML to text
     html_tree = etree.HTML(text)
     if html_tree is not None:
         text = html_tree.xpath('string(.)')
     else:
-        text = ""  # 如果转换失败，返回空字符串
-    # 去除数字、字母、空白符以外的所有字符（例如表情）
+        text = ""  # Return an empty string if the conversion fails
+    # Remove all characters except numbers, letters, and whitespace (e.g., emojis)
     text = re.sub(r"[^\w\s]|\_", " ", text)
-    # 提取词干
+    # Extract word stems
     text = " ".join([EnglishStemmer().stem(w) for w in text.split()])
-    # 去除停用词
+    # Remove stopwords
     text = " ".join([w for w in text.split() if w not in set(stopwords.words("english"))])  
 
     return text
