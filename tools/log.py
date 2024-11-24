c29bf8b748cc9f46cf706f914c1359730350732d
zhingoll <z1823810655@outlook.com>
2024-11-24 20:33:33 +0800

    modify comments

--- tools/log.py
+++ tools/log.py
@@ -6,32 +6,32 @@ class Log():
         self.logger = logging.getLogger(model_name)
         self.logger.setLevel(level=logging.DEBUG)
 
-        # 创建日志目录
+        # Create a directory for logs
         log_dir = './log/'
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
 
-        # 设置日志文件
+        # Set the log file
         log_file = os.path.join(log_dir, model_name_current_time + '.log')
         
-        # 清除已存在的日志句柄
+        # Remove existing log handlers
         if self.logger.handlers:
             for handler in self.logger.handlers:
                 self.logger.removeHandler(handler)
 
-        # 文件日志
+        # File logger
         file_handler = logging.FileHandler(log_file)
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 
-        # 控制台日志
+        # Console logger
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
         console_handler.setFormatter(logging.Formatter(
         '\033[0;32m%(asctime)s - %(name)s - \033[0;34m%(levelname)s - \033[0m%(message)s', 
         datefmt='%Y-%m-%d %H:%M:%S'))
 
-        # 添加句柄到日志器
+        # Add handlers to the logger
         self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
 
